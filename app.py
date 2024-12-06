c0f0904366eaafae58395a25f0cc26f33ef4b1e1
maishatasnim25 <maisha.tasnim1@g.bracu.ac.bd>
2024-12-06 20:14:03 +0600

    User Class

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
+import os
 from flask import Flask
+from models import db  
+from models.user import User  
+
 
 app = Flask(__name__)
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
+
+if not os.path.exists('instance'):
+    os.makedirs('instance')
+
+
+db.init_app(app)
+
+
+with app.app_context():
+    print("Creating tables...")
+    db.create_all()  
+    print("Tables created!")
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return "Flask app is running!"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
---
+++ models/user.py
@@ -0,0 +1,33 @@
+from . import db  
+
+
+class User(db.Model):
+    __tablename__ = 'user'  
+
+    
+    userid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    nid = db.Column(db.String(50), unique=True, nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    address = db.Column(db.String(200), nullable=True)
+    password = db.Column(db.String(200), nullable=False)
+    contact = db.Column(db.String(20), nullable=False)
+    blood_type = db.Column(db.String(5), nullable=True)
+
+   
+    def __init__(self, username, name, nid, date_of_birth, email, address, password, contact, blood_type):
+        self.username = username
+        self.name = name
+        self.nid = nid
+        self.date_of_birth = date_of_birth
+        self.email = email
+        self.address = address
+        self.password = password
+        self.contact = contact
+        self.blood_type = blood_type
+
+   
+    def __repr__(self):
+        return f'<User {self.username}>'
