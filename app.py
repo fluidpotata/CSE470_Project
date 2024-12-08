201d0d40333bbc6a76064864cbbc2584464d9178
Alif <md.ibrahim.alif@g.bracu.ac.bd>
2024-12-09 01:23:15 +0600

    Fix authenticate

--- app.py
+++ app.py
@@ -24,7 +24,16 @@ def login():
             return "Success" #User class creation here
         else:
             # return render_template('login.html', is_success=is_success)
-            return "rejected"
+            return ("rejected")
+
+
+@app.route('/signup')
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        pass # will be done by mystics
+
 
 if __name__ == '__main__':
     app.run(debug=True)
