762fdf736d6cf420c632df1ebe6eca5dd1833d4d
Md Ibrahim Alif <md.ibrahim.alif@g.bracu.ac.bd>
2024-12-16 12:44:08 +0600

    Create User Class upon login

--- models/user.py
+++ models/user.py
@@ -21,10 +21,15 @@ class User():
     @staticmethod
     def authenticate(username, password):
         query = f"SELECT password FROM users WHERE username='{username}';"
-
-        if password==get_result_from_query(query)[0][0]:
-            return True
-        return False
+        query2 = f'''select count(*) from users where username='{username}';'''
+        try:
+            if password==get_result_from_query(query)[0][0]:
+                res = get_result_from_query(query2)
+                user = User(res[0][0], res[0][2], res[0][3], res[0][4], res[0][5], res[0][6], res[0][7], res[0][8], res[0][9])
+                return (True, user)
+        except:
+            pass
+        return (False, None)
         # object creation and session creation is yet to be done
 
     @staticmethod
